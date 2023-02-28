591968a29a80f0320fde1c0346256aa5f51aa2b1
estanis22 <estanislaoperezpesado@gmail.com>
2023-02-28 14:09:04 -0300

    agregando algunos autos

--- autos.js
+++ autos.js
@@ -33,6 +33,83 @@ let autos = [
         patente : "JCD221",
         vendido : false
     },
+    {
+      marca : "Chevrolet",
+      modelo : "Onix",
+      precio: 200000, 
+      km : 100,
+      color : "Rojo",
+      cuotas : 24,
+      anio : 2021,
+      patente : "ABD123",
+      vendido : false
+    },
+    {
+      marca : "Volkswagen",
+      modelo : "Gol",
+      precio : 120000, 
+      km : 5000,
+      color : "Negro",
+      cuotas : 10,
+      anio : 2018,
+      patente : "ACF345",
+      vendido : false
+    },
+    {
+      marca : "Renault",
+      modelo : "Clio",
+      precio: 100000, 
+      km : 80000,
+      color : "Azul",
+      cuotas : 12,
+      anio : 2015,
+      patente : "KAD121",
+      vendido : false
+    },
+    {
+      marca : "Fiat",
+      modelo : "Palio",
+      precio: 85000, 
+      km : 150000,
+      color : "Verde",
+      cuotas : 6,
+      anio : 2013,
+      patente : "FRD981",
+      vendido : false
+    },
+    {
+      marca : "Peugeot",
+      modelo : "208",
+      precio: 170000, 
+      km : 20000,
+      color : "Plateado",
+      cuotas : 18,
+      anio : 2020,
+      patente : "JLM214",
+      vendido : false
+    },
+    {
+      marca : "Nissan",
+      modelo : "Versa",
+      precio: 180000, 
+      km : 3000,
+      color : "Blanco",
+      cuotas : 12,
+      anio : 2022,
+      patente : "MLK989",
+      vendido : false
+    },
+    {
+      marca : "Hyundai",
+      modelo : "i10",
+      precio: 90000, 
+      km : 50000,
+      color : "Gris",
+      cuotas : 6,
+      anio : 2016,
+      patente : "LPQ456",
+      vendido : false
+    }
 ];
 
 module.exports = autos;
